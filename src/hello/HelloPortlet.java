53e11d031e49c5fdc26962e04d00d3722c146585
serverkabure <serverkabure@live.jp>
2012-06-10 00:43:18 +0900

    portlet.xmlのパラメータでview,edit,helpを指定できるように変更。 DB接続ライブラリ追加。

--- src/hello/HelloPortlet.java
+++ src/hello/HelloPortlet.java
@@ -3,18 +3,30 @@ package hello;
 import java.io.IOException;
 
 import javax.portlet.GenericPortlet;
+import javax.portlet.PortletConfig;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequestDispatcher;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 public class HelloPortlet extends GenericPortlet {
+	private String viewPage = "/jsp/view.jsp";
+	private String editPage = "/jsp/edit.jsp";
+	private String helpPage = "/jsp/help.jsp";
+
+	@Override
+	public void init(PortletConfig config) throws PortletException {
+		super.init(config);
+		this.viewPage = config.getInitParameter("viewPage");
+		this.editPage = config.getInitParameter("editPage");
+		this.helpPage = config.getInitParameter("helpPage");
+	}
 
 	@Override
 	protected void doView(RenderRequest request, RenderResponse response)
 			throws PortletException, IOException {
 		PortletRequestDispatcher dispatcher = getPortletContext()
-				.getRequestDispatcher("/jsp/view.jsp");
+				.getRequestDispatcher(viewPage);
 		dispatcher.include(request, response);
 	}
 
@@ -22,7 +34,7 @@ public class HelloPortlet extends GenericPortlet {
 	protected void doEdit(RenderRequest request, RenderResponse response)
 			throws PortletException, IOException {
 		PortletRequestDispatcher dispatcher = getPortletContext()
-				.getRequestDispatcher("/jsp/edit.jsp");
+				.getRequestDispatcher(editPage);
 		dispatcher.include(request, response);
 	}
 
@@ -30,7 +42,7 @@ public class HelloPortlet extends GenericPortlet {
 	protected void doHelp(RenderRequest request, RenderResponse response)
 			throws PortletException, IOException {
 		PortletRequestDispatcher dispatcher = getPortletContext()
-				.getRequestDispatcher("/jsp/help.jsp");
+				.getRequestDispatcher(helpPage);
 		dispatcher.include(request, response);
 	}
 
